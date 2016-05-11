8bcaf154e7470125780d5778bd1a00926ecb0067
gocha <gochaism@gmail.com>
2016-05-11 23:35:17 +0900

    Update the version number to v0.1 for the first beta release

--- include/sf2cute/version.hpp
+++ include/sf2cute/version.hpp
@@ -7,6 +7,10 @@
 #define SF2CUTE_VERSION_HPP_
 
 /// The version number of SoundFont 2 library.
-#define SF2CUTE_VERSION 1
+///
+/// SF2CUTE_VERSION % 100 is the patch level. \n
+/// SF2CUTE_VERSION / 100 % 1000 is the minor version. \n
+/// SF2CUTE_VERSION / 100000 is the major version.
+#define SF2CUTE_VERSION 100
 
 #endif // SF2CUTE_VERSION_HPP_
